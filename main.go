3fe438ee29e8f87513dc1e63000fda5e10097c72
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-03-05 22:21:00 +0700

    feat: gorm get data table

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crowdfunding_app/user"
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -9,11 +10,21 @@ import (
 
 func main() {
 	dsn := "root:Horse_6007@tcp(127.0.0.1:3306)/db_crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connected to database")
+
+	var users []user.User
+
+	db.Find(&users)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+		fmt.Println("=============")
+	}
 }
