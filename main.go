c858016be5b8b404ca4d119f963630f6bfbe100f
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-03-06 21:50:12 +0700

    feat: login handler

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"crowdfunding_app/handler"
 	"crowdfunding_app/user"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -21,26 +20,13 @@ func main() {
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
 
-	input := user.LoginInput{
-		Email:    "gusti@gmail.com",
-		Password: "password",
-	}
-
-	user, err := userService.Login(input)
-	if err != nil {
-		fmt.Println("Terjadi kesalahan")
-		fmt.Println(err.Error())
-	}
-
-	fmt.Println(user.Email)
-	fmt.Println(user.Name)
-
 	userHandler := handler.NewUserHandler(userService)
 
 	router := gin.Default()
 	api := router.Group("/api/v1")
 
 	api.POST("/users", userHandler.RegisterUser)
+	api.POST("/sessions", userHandler.Login)
 
 	router.Run(":9000")
 }
