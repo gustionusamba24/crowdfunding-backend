6bb3ac4acd74bf7c00fbe74c7f96d4d3e38d5198
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-03-12 15:44:12 +0700

    feat: upload campaign image authorization

--- handler/campaign.go
+++ handler/campaign.go
@@ -129,6 +129,10 @@ func (h *campaignHandler) UploadImage(c *gin.Context) {
 		return
 	}
 
+	currentUser := c.MustGet("currentUser").(user.User)
+	input.User = currentUser
+	userID := currentUser.ID
+
 	file, err := c.FormFile("file")
 	if err != nil {
 		data := gin.H{
@@ -138,9 +142,6 @@ func (h *campaignHandler) UploadImage(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
-
-	currentUser := c.MustGet("currentUser").(user.User)
-	userID := currentUser.ID
 
 	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
 
