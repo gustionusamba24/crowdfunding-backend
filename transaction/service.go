c52c1756d7d7c495da9d17fd932be8c0d3808ac3
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-03-12 21:58:23 +0700

    feat: campaign transaction service

--- transaction/service.go
+++ transaction/service.go
@@ -1,7 +1,7 @@
 package transaction
 
 type Service interface {
-	GetTransactionByCampaignID(campaignID int) ([]Transaction, error)
+	GetTransactionByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error)
 }
 
 type service struct {
@@ -12,8 +12,8 @@ func NewService(repository Repository) *service {
 	return &service{repository}
 }
 
-func (s *service) GetTransactionByCampaignID(campaignID int) ([]Transaction, error) {
-	transactions, err := s.repository.GetByCampaignID(campaignID)
+func (s *service) GetTransactionByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error) {
+	transactions, err := s.repository.GetByCampaignID(input.ID)
 	if err != nil {
 		return transactions, err
 	}
