14ab10ea879a56f8f42d5150847465bccc02c573
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-03-12 22:20:31 +0700

    feat: campaign transaction handler

--- handler/transaction.go
+++ handler/transaction.go
@@ -1,1 +1,37 @@
 package handler
+
+import (
+	"crowdfunding_app/helper"
+	"crowdfunding_app/transaction"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type transactionHandler struct {
+	service transaction.Service
+}
+
+func NewTransactionHandler(service transaction.Service) *transactionHandler {
+	return &transactionHandler{service}
+}
+
+func (h *transactionHandler) GetCampaignTransactions(c *gin.Context) {
+	var input transaction.GetCampaignTransactionsInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign's transactions", http.StatusBadRequest, "failed", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	transactions, err := h.service.GetTransactionByCampaignID(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign's transactions", http.StatusBadRequest, "failed", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Campaign detail", http.StatusOK, "success", transactions)
+	c.JSON(http.StatusOK, response)
+}
